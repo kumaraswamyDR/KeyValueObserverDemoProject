bb0ee4eaa294e37fee9fd7c87d891b86ea0de8b8
kumaraswamyDR <kumaraswamyDR@kumarmac.local>
2016-08-29 01:09:53 +0530

    Added KVO project

---
+++ KeyValueObserverDemoProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KeyValueObserverDemoProject
+//
+//  Created by Kumaraswamy D R on 29/08/16.
+//  Copyright © 2016 TechLeraner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController 
+
+
+@end
+
---
+++ KeyValueObserverDemoProject/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  KeyValueObserverDemoProject
+//
+//  Created by Kumaraswamy D R on 29/08/16.
+//  Copyright © 2016 TechLeraner. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
